83c79920462a28023a6b5b8238bb9b2eb16c72e3
caobo410 <caobo410@163.com>
2016-09-18 17:21:23 +0800

    修改基础信息

--- fwxt_jcsj/models/models.py
+++ fwxt_jcsj/models/models.py
@@ -16,6 +16,21 @@ from datetime import datetime
 date_ref = datetime.now().strftime('%Y-%m-%d')
 _logger = logging.getLogger(__name__)
 
+
+def _get_select_materia_types(self):
+    return self.pool.get('dict.info').get_selection_type_items('material', self._cr, self._uid)
+
+def _get_select_commodity_types(self):
+    return self.pool.get('dict.info').get_selection_type_items('commodity', self._cr, self._uid)
+
+def _get_select_supplier_types(self):
+    return self.pool.get('dict.info').get_selection_type_items('supplier', self._cr, self._uid)
+
+
+#字典查询下拉菜单
+
+
+#企业信息
 class company_info(models.Model):
     _name = "company.info"
     _description = "company.info"
@@ -32,6 +47,7 @@ class company_info(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
     }
 
+#照片管理
 class picture_management(models.Model):
     _name = "picture.management"
     _description = "picture.management"
@@ -50,6 +66,7 @@ class picture_management(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
     }
 
+#单位换算
 class convert_info(models.Model):
     _name = "convert.info"
     _description = "convert.info"
@@ -66,6 +83,8 @@ class convert_info(models.Model):
         'date_confirm': date_ref,
         'user_id': lambda cr, uid, id, c={}: id,
     }
+
+#视频管理
 class video_info(models.Model):
     _name = "video.info"
     _description = "video.info"
@@ -86,49 +105,46 @@ class video_info(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
     }
 
+#字典信息
 class dict_info(models.Model):
     _name = "dict.info"
     _description = "dict.info"
 
-    code = fields.Char(string='Code', size=64, readonly=True, required=True, help="Code")
-    name = fields.Char(string='Name', size=64, required=True, help="Name")
-    type = fields.Selection([('supplier', 'Supplier'),
-                             ('dealer','Dealer'),
-                             ('distributor','Distributor'),
-                             ('detection','Detection'),
-                             ('production','Producttion'),
-                             ('material','Material'),
-                             ('manufacturing','Manufacturing'),
-                             ('warehouse','Warehouse')], 'Type', required=True, help="Type")
-    dist = fields.Char(string='Dist', help="Dist")
-    user_id = fields.Many2one('res.users', string='Operator')
-    date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
-
-    _defaults = {
-        'date_confirm': date_ref,
-        'user_id': lambda cr, uid, id, c={}: id,
-    }
-
-class material_type(models.Model):
-    _name = "materia.type"
-    _description = "materia.type"
-
     code = fields.Char(string='Code', size=64, required=True, help="Code")
     name = fields.Char(string='Name', size=64, required=True, help="Name")
+    type = fields.Selection([('material', 'Material'),
+                             ('supplier', 'Supplier'),
+                             ('commodity', 'Cmmodity')], 'Type', required=True, help="Type")
+    dict = fields.Char(string='Dict', help="Dict")
     user_id = fields.Many2one('res.users', string='Operator')
     date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
+
+    def get_selection_type_items(self, type, cr, uid):
+        dicts = self.search(cr, uid, [('type', '=', type)])
+        if not len(dicts):
+            return []
+        ret_list = []
+        for dict in dicts:
+            item = self.browse(cr, uid, dict)
+            ret_list.extend([(item.code, item.name)])
+        return ret_list
 
     _defaults = {
         'date_confirm': date_ref,
         'user_id': lambda cr, uid, id, c={}: id,
     }
+
+#原材料
 class material_info(models.Model):
     _name = "materia.info"
     _description = "materia.info"
 
     code = fields.Char(string='Code', size=64, required=True, help="Code")
     name = fields.Char(string='Name', size=64, required=True, help="Name")
-    materia_type = fields.Many2one('materia.type', string='Type')
+    type = fields.Selection(selection=_get_select_materia_types, string='Type', required=True)
+    # type = fields.Selection([('material', 'Material'),
+    #                          ('supplier', 'Supplier'),
+    #                          ('commodity', 'Cmmodity')], 'Type', required=True, help="Type")
     message = fields.Char(string='Message', help="Message")
     user_id = fields.Many2one('res.users', string='Operator')
     date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
@@ -138,13 +154,14 @@ class material_info(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
     }
 
+# 生产基地
 class branch_office_info(models.Model):
     _name = "branch.office.info"
     _description = "branch.office.info"
 
     code = fields.Char(string='Code', size=64, required=True, help="Code")
     name = fields.Char(string='Name', size=64, required=True, help="Name")
-    video_info = fields.Many2one('video.info', string='Video Info')
+    video_id = fields.Many2one('video.info', string='Video Info')
     message = fields.Char(string='Message', help="Message")
     user_id = fields.Many2one('res.users', string='Operator')
     date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
@@ -154,42 +171,17 @@ class branch_office_info(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
     }
 
-class material_info(models.Model):
-    _name = "materia.info"
-    _description = "materia.info"
-
-    code = fields.Char(string='Code', size=64, required=True, help="Code")
-    name = fields.Char(string='Name', size=64, required=True, help="Name")
-    materia_type = fields.Many2one('materia.type', string='Type')
-    message = fields.Char(string='Message', help="Message")
-    user_id = fields.Many2one('res.users', string='Operator')
-    date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
-
-    _defaults = {
-        'date_confirm': date_ref,
-        'user_id': lambda cr, uid, id, c={}: id,
-    }
-
-class commodity_type(models.Model):
-    _name = "commodity.type"
-    _description = "commodity.type"
-
-    code = fields.Char(string='Code', size=64, required=True, help="Code")
-    name = fields.Char(string='Name', size=64, required=True, help="Name")
-    user_id = fields.Many2one('res.users', string='Operator')
-    date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
-
-    _defaults = {
-        'date_confirm': date_ref,
-        'user_id': lambda cr, uid, id, c={}: id,
-    }
+#成品商品
 class commodity_info(models.Model):
     _name = "commodity.info"
     _description = "commodity.info"
 
     code = fields.Char(string='Code', size=64, required=True, help="Code")
     name = fields.Char(string='Name', size=64, required=True, help="Name")
-    commodity_type = fields.Many2one('commodity.type', string='Type')
+    # type = fields.Selection(selection=_get_select_commodity_types, string='Type', required=True)
+    type = fields.Selection([('material', 'Material'),
+                             ('supplier', 'Supplier'),
+                             ('commodity', 'Cmmodity')], 'Type', required=True, help="Type")
     message = fields.Char(string='Message', help="Message")
     user_id = fields.Many2one('res.users', string='Operator')
     date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
@@ -198,19 +190,8 @@ class commodity_info(models.Model):
         'date_confirm': date_ref,
         'user_id': lambda cr, uid, id, c={}: id,
     }
-class supplier_type(models.Model):
-    _name = "supplier.type"
-    _description = "supplier.type"
 
-    code = fields.Char(string='Code', size=64, required=True, help="Code")
-    name = fields.Char(string='Name', size=64, required=True, help="Name")
-    user_id = fields.Many2one('res.users', string='Operator')
-    date_confirm = fields.Date(string='Date', size=64, required=True, help="Date")
-
-    _defaults = {
-        'date_confirm': date_ref,
-        'user_id': lambda cr, uid, id, c={}: id,
-    }
+#供应商
 class supplier_info(models.Model):
     _name = "supplier.info"
     _description = "supplier.info"
@@ -221,7 +202,10 @@ class supplier_info(models.Model):
                              ('agent', 'Agent'),
                              ('express', 'Express')], 'Product Type', required=True, help="Type")
 
-    supplier_type = fields.Many2one('supplier.type', string='Type')
+    # supplier_type = fields.Selection(selection=_get_select_supplier_types, string='Supplier type', required=True)
+    supplier_type = fields.Selection([('material', 'Material'),
+                                      ('supplier', 'Supplier'),
+                                      ('commodity', 'Cmmodity')], 'Type', required=True, help="Type")
     contacts_name = fields.Char(string='Contacts Name', help="Contacts Name")
     tel = fields.Char(string='Tel', help="Tel")
     address = fields.Char(string='Address', size=64, required=True, help="Address")
@@ -234,4 +218,4 @@ class supplier_info(models.Model):
         'user_id': lambda cr, uid, id, c={}: id,
         'type': 'supplier'
     }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
