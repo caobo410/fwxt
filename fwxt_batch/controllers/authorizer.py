ba8b22a504ccff7a271a6a466f92763b405270ce
xiaocao <caobo410@163.com>
2017-11-12 17:07:45 +0800

    修改admin登陆信息

--- fwxt_batch/controllers/authorizer.py
+++ fwxt_batch/controllers/authorizer.py
@@ -10,7 +10,7 @@ def authorize(f):
     @functools.wraps(f)
     def wrap(controller, **kwargs):
         try:
-            env = rest.check_token('fwxt', 'admin', '1')
+            env = rest.check_token('fwxt', 'admin', 'hqfw18663799361')
             if not env:
                 return rest.unauthorized()
             else:
